39fdc3f4d396f51407c4b263f9f886ef82fedeae
unknown <purshottamkumarmishra44@gmail.com>
2024-09-28 02:01:50 +0530

    second

--- index.js
+++ index.js
@@ -4,15 +4,11 @@ const server=jsonServer.create();
 
 const router=jsonServer.router("db.json");
 
-const middlewares = jsonServer.defaults({
-    static:"./build"
-})
+const middlewares = jsonServer.defaults();
 
 const port =process.env.PORT || 5000;
 
 server.use(middlewares);
 
 server.use(router);
-server.listen(port,()=>{
-    console.log(`server is running on port ${port}`);
-})
+server.listen(port);
